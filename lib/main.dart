502b096be32089d2178ec411428e206597cdae33
Ali Ramadan <alir00330@gmail.com>
2023-04-15 15:32:32 +0200

    inital project created empty home screen

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:islamiapp/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
